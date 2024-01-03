1f9e77878ad60a215079c160c3a0a23f14bf7d1f
iRaziul <raziul.cse@gmail.com>
2024-01-03 15:31:55 +0600

    helper functions

---
+++ app/Helpers/Functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function blood_groups(): array
+{
+    return [
+        'A+' => 'A+',
+        'A-' => 'A-',
+        'B+' => 'B+',
+        'B-' => 'B-',
+        'AB+' => 'AB+',
+        'AB-' => 'AB-',
+        'O+' => 'O+',
+        'O-' => 'O-',
+    ];
+}
