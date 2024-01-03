7778240f36232e5e81999fe9a001cbca969d76a7
iRaziul <raziul.cse@gmail.com>
2024-01-03 14:33:58 +0600

    isolate auth.php

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    Route::view('profile', 'profile')->name('profile');
+    Route::view('dashboard', 'dashboard')->name('dashboard')->middleware('verified');
+});
