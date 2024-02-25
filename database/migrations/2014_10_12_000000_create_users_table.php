90fe3d951f30774bdcb5c1f5f09ef9b54f4e7501
iRaziul <raziul.cse@gmail.com>
2024-02-26 01:49:49 +0600

    user fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,20 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('bio')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 20)->nullable();
             $table->string('password');
-
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->date('birthday')->nullable();
+            $table->string('blood_group', 4)->nullable();
+            $table->string('image_path')->nullable();
             $table->timestamp('suspended_at')->nullable();
             $table->timestamp('suspension_ends_at')->nullable();
             $table->string('suspension_reason')->nullable();
-
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
