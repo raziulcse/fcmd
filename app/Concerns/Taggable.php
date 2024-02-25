d6d806c5decac4d339b48864dc292b16915c935b
iRaziul <raziul.cse@gmail.com>
2024-02-25 12:15:39 +0600

    Refactor to `Taggable`

--- app/Concerns/Taggable.php
+++ app/Concerns/Taggable.php
@@ -7,11 +7,14 @@ use ArrayAccess;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
-trait HasTags
+trait Taggable
 {
     protected array $queuedTags = [];
 
-    public static function bootHasTags()
+    /**
+     * Boot the trait.
+     */
+    public static function bootTaggable()
     {
         static::created(function (Model $model) {
             if (count($model->queuedTags) === 0) {
@@ -30,6 +33,9 @@ trait HasTags
         });
     }
 
+    /**
+     * Set the tags attribute.
+     */
     public function setTagsAttribute(string|array|ArrayAccess|Tag $tags)
     {
         if (! $this->exists) {
@@ -41,16 +47,25 @@ trait HasTags
         $this->syncTags($tags);
     }
 
+    /**
+     * Get the tags relationship.
+     */
     public function tags(): MorphToMany
     {
         return $this->morphToMany(Tag::class, 'taggable');
     }
 
+    /**
+     * Sync provided tags with the model.
+     */
     public function syncTags(array $tags)
     {
         $this->tags()->sync($tags);
     }
 
+    /**
+     * Attach tags to the model.
+     */
     public function attachTags(array|ArrayAccess|Tag $tags): static
     {
         $tags = collect(Tag::findOrCreate($tags));
@@ -60,6 +75,9 @@ trait HasTags
         return $this;
     }
 
+    /**
+     * Detach tags from the model.
+     */
     public function detachTags(array $tags = null)
     {
         $this->tags()->detach($tags);
